d5077f9a3f0c4739430c5577fb1946574b25dc7a
hangedplane <hangedplane@gmail.com>
2017-09-02 19:08:07 +0300

    update desktop version
    fixed jar file

---
+++ SapperDesktopVersion/src/UI/UIElements/LevelDifficulty.java
@@ -0,0 +1,13 @@
+package UI.UIElements;
+
+/**
+ * Created by @author Telnov Sergey on 02.09.2017.
+ */
+public enum LevelDifficulty {
+    BEGINNER,
+    EASY,
+    NORMAL,
+    HARD,
+    INTENSE,
+    NO_LEVEL
+}
